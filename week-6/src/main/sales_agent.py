eb46a592112242b0f0d452f3f6ba2554d979b762
krishnagopika <krishnagopika1701@gmail.com>
2024-02-10 02:27:49 +0530

    re arranged the files

--- week-6/src/main/sales_agent.py
+++ week-6/src/main/sales_agent.py
@@ -1,17 +1,17 @@
 from langchain.agents import ConversationalChatAgent, AgentExecutor
-import vectordb_tool
+from tools.vectordb_tool import get_vectordb_tool
 from model import get_chat_model
 from agent_chat_memory import get_memory
 from sales_agent_prompt import sales_agent_prompt
 from langchain.agents import load_tools
 from model import get_llm
-from general_tool import get_general_tool
+from tools.general_tool import get_general_tool
 
 
 
 def get_tools():
     tools = load_tools([], llm = get_llm('llama2'))
-    tools.append(vectordb_tool.get_vectordb_tool())
+    tools.append(get_vectordb_tool())
     tools.append(get_general_tool())
     return tools
 
